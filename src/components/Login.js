6747f7339bb9d47ae9f11c9e6b8dacdeaa78c70e
Dalecia <petersondalecia@gmail.com>
2024-05-11 00:10:40 -0400

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 function Login() {
+  // Access the login function passed as context
+  const login = useOutletContext();
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -11,12 +14,15 @@ function Login() {
       ...formData,
       [e.target.name]: e.target.value,
     });
-  }
+  };
 
+  // Create a function that calls the login function when the form is submitted
   function handleLogin(e) {
     e.preventDefault();
-  }
+    login();
+  };
 
+  
   return (
     <form onSubmit={handleLogin}>
       <label for="username">Username</label>
@@ -40,8 +46,8 @@ function Login() {
         />      
       </div>
       <button type="submit">Login</button>
-    </form>
+  </form>
   );
-}
+};
 
 export default Login;
